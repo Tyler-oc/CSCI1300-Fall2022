07ba54b1b5083f558f9e6ee7bde49dc409cb84f7
Tyler Onstad-Cran <84589084+Tyler-oc@users.noreply.github.com>
2022-09-11 14:13:23 -0600

    Add files via upload

---
+++ homework/helloWorld.cpp
@@ -0,0 +1,16 @@
+// CSCI 1300 Fall 2022
+// Author: Tyler Onstad-Cran
+// Recitation: 305 – Nikhith Sannidhi
+// Homework 2 - Problem #1
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    //output phrase
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
